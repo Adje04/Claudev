5f0a213d3372776c0e30bb39fc2a6648fe0bda92
Adje04 <adjeeklou22@gmail.com>
2025-08-03 12:35:48 +0000

    indexation-file

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -10,8 +10,33 @@ const inter = Inter( {subsets: ["latin"],} );
 
 
 export const metadata: Metadata = {
-  title: "Claude-developper",
-  description: "developper minimalist portfolio",
+  title: "Adjé Claude – Développeur fullstack",
+  description:
+    "KPONON-EKLOU Adjé Claude – Développeur fullstack passionné. Expert Laravel, Node.js et Next.js. Je transforme vos idées en solutions digitales concrètes et performantes.",
+  openGraph: {
+    title: "Adjé Claude – Développeur Backend",
+    description:
+      "Développeur fullstack spécialisé en Laravel, Node.js et Next.js. Découvrez mes projets et solutions digitales sur mon portfolio.",
+    url: "https://claudev-woad.vercel.app",
+    siteName: "Adjé Claude Developer",
+    images: [
+      {
+        url: "/og-image.png", 
+        width: 1200,
+        height: 630,
+        alt: "Portfolio de Adjé Claude Développeur",
+      },
+    ],
+    locale: "fr_FR",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Adje Claude – Développeur Backend",
+    description:
+      "Portfolio de KPONON-EKLOU Adjé Claude – Développeur Laravel, Node.js, Next.js. Projets, solutions digitales, expertise technique.",
+    images: ["/og-image.png"],
+  },
 };
 
 export default async function RootLayout({
